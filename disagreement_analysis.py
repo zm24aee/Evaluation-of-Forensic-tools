5c126c21aaacb8cdfb3f1b720d0c595ac74c88f9
Muneeba <zm24aee@herts.ac.uk>
2026-04-27 01:34:25 +0100

    Update disagreement_analysis.py
    additional changes

--- disagreement_analysis.py
+++ disagreement_analysis.py
@@ -6,10 +6,8 @@ MEM_PATH = r"C:\Forensic\MemProcFS_Results\master_summary.json"
 
 with open(VOL_PATH, "r", encoding="utf-8") as f:
     vol = json.load(f)["results"]
-
 with open(MEM_PATH, "r", encoding="utf-8") as f:
     mem = json.load(f)["results"]
-
 for run in sorted(vol.keys()):
     vol_found = {x["artefact_id"] for x in vol[run].get("true_detections", []) if "artefact_id" in x}
     mem_found = {x["artefact_id"] for x in mem[run].get("true_detections", []) if "artefact_id" in x}
